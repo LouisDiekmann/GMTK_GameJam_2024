54018f7ce7d9df31a7ea92a7db74a1edc2555c0e
Louis <diekmann.lv@gmail.com>
2024-08-17 10:09:51 +0200

    customer animations and textLines

--- Scripts/customer.gd
+++ Scripts/customer.gd
@@ -2,10 +2,15 @@ extends Node3D
 class_name Customer
 
 @export var characterSprite: Sprite3D
+@onready var object: Node3D = $object
+@onready var objectSizeVectorLength : float = object.scale.length()
+@onready var animationPlayer: AnimationPlayer = $characterSprite/AnimationPlayer
+@onready var audioStreamPlayer: AudioStreamPlayer = $AudioStreamPlayer
+
 
 var characters : Array[CompressedTexture2D]
 var voice : Array
-var object : Array[MeshInstance3D]
+var objects : Array[MeshInstance3D]
 var prompt : Array
 
 # Called when the node enters the scene tree for the first time.
@@ -13,11 +18,16 @@ func _ready() -> void:
 	setCharacterSprite()
 	setCharacterVoice()
 	setObject()
+	await get_tree().create_timer(.5).timeout
+	playAnimation()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
+	if Input.is_action_pressed("enlarge") and object.scale.x < 2:
+		object.scale += Vector3(.01,.01,.01)
+	if Input.is_action_pressed("shrink") and object.scale.x > .5:
+		object.scale -= Vector3(.01,.01,.01)
+		
 func _init() -> void:
 	pass
 	
@@ -37,3 +47,13 @@ func setCharacterVoice() -> void:
 
 func setObject() -> void:
 	pass
+	
+func playAnimation() -> void:
+	animationPlayer.play("swoopIn")
+	audioStreamPlayer.pitch_scale = randf_range(.5,1.5)
+	audioStreamPlayer.play()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "swoopIn":
+		animationPlayer.play("wobble")
+	
