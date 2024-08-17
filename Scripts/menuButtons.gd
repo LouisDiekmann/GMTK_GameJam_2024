7a1934b232c2a3569867c8469391f9623c156c33
Louis <diekmann.lv@gmail.com>
2024-08-17 22:36:00 +0200

    second character

--- Scripts/menuButtons.gd
+++ Scripts/menuButtons.gd
@@ -2,6 +2,8 @@ extends VBoxContainer
 
 @export var menuNode : CanvasLayer
 @export var mainNode : Node3D
+@onready var paper: AudioStreamPlayer = $paper
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,9 +14,17 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_down() -> void:
+	paper.play()
 	menuNode.openCurtains()
 	mainNode.startGame()
 
 
 func _on_credits_button_down() -> void:
+	paper.play()
 	menuNode.panel.visible = not menuNode.panel.visible
+
+func _on_settings_button_down() -> void:
+	paper.play()
+
+func _on_exit_button_down() -> void:
+	get_tree().quit()
